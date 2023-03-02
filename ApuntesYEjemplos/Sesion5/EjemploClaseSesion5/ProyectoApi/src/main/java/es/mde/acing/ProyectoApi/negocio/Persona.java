3b74d0538ba80f0b07e4aa0548e3348811517b93
jlPortus <jl_puerto@hotmail.com>
2023-03-02 08:52:13 +0100

    Creo la persona plana

---
+++ ApuntesYEjemplos/Sesion5/EjemploClaseSesion5/ProyectoApi/src/main/java/es/mde/acing/ProyectoApi/negocio/Persona.java
@@ -0,0 +1,63 @@
+package es.mde.acing.ProyectoApi.negocio;
+
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+//@JsonPropertyOrder({ "edad", "nombre" })
+public class Persona {
+
+	String nombre;
+//	@JsonIgnore
+	int edad;
+
+//	Date fechaNacimiento;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+//	public Date getFechaNacimiento() {
+//		return fechaNacimiento;
+//	}
+//
+//	public void setFechaNacimiento(Date fechaNacimiento) {
+//		this.fechaNacimiento = fechaNacimiento;
+//	}
+
+	public Persona() {
+
+	}
+
+	public Persona(String nombre, int edad, Date fechaNacimiento) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+//		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	@Override
+	public String toString() {
+		return "Mi Objeto Persona se llama " + nombre 
+				+ " y tiene " + edad 
+//				+ ", fechaNacimiento=" + fechaNacimiento 
+				+ " años.";
+	}
+
+	
+	
+}
