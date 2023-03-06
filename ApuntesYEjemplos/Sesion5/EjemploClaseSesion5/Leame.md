9022263b819f3f32b613da1bcc152bb7ad8990c0
jlPortus <jl_puerto@hotmail.com>
2023-03-06 12:01:53 +0100

    Incluyendo el proyecto libreria en repo

---
+++ ApuntesYEjemplos/Sesion5/EjemploClaseSesion5/Leame.md
@@ -0,0 +1,6 @@
+En esta carpeta deben estar los proyecto api y libreria
+
+El proyecto libreria puede no aparecer, por tener su propio repositorio git
+-> En ese caso hacer un clonado en esta carpeta del repositorio 
+
+https://github.com/jlportus/ProyectoLib
---
+++ ApuntesYEjemplos/Sesion5/EjemploClaseSesion5/ProyectoLib/src/main/java/es/mde/acing/ProyectoLib/utils/ClasePruebaLibreria.java
@@ -0,0 +1,13 @@
+package es.mde.acing.ProyectoLib.utils;
+
+public class ClasePruebaLibreria {
+
+	/*
+	 * metodo de prueba que permite comprobar que la libreria esta enlazada
+	 * 
+	 * @author jlportus
+	 */
+	public static void holaLibreria() {
+		System.out.println("Hola desde la libreria");
+	}
+}
